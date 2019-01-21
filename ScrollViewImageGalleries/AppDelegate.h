b5a66d373d703c7d7c814bae3d77e01d4fccd260
Jenny Chang <jennychang@jennys-macbook-pro.local>
2019-01-21 15:23:54 -0800

    Initial Commit

---
+++ ScrollViewImageGalleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageGalleries
+//
+//  Created by Jenny Chang on 21/01/2019.
+//  Copyright © 2019 Jenny Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
